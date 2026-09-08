629cd7bfea56581c09a227e1eca7aa2479a59bc2
twenityelaa <naghelaarniesa62@gmail.com>
2026-09-08 14:04:37 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
